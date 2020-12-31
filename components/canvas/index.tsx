c128c4c5931f39793c5e3c0426dc761c7f0d68a4
Jordan Rein <makeitrein@gmail.com>
2020-12-30 19:51:45 -0800

    Type puzzle partial

--- components/canvas/index.tsx
+++ components/canvas/index.tsx
@@ -1,16 +1,27 @@
 import * as React from "react";
 import { MovableCard } from "./movable-card";
 
+const movableCards = [{ key: 1 }, { key: 2 }, { key: 3 }];
+
 export function Canvas() {
   const [target, setTarget] = React.useState();
   const [elementGuidelines, setElementGuidelines] = React.useState([]);
 
+  const refs = React.useMemo(
+    () =>
+      Array.from({ length: movableCards.length }, () =>
+        React.createRef<HTMLDivElement>()
+      ),
+    [movableCards]
+  );
+
+  console.log(refs);
+
   return (
     <div className="container">
-      <MovableCard />
-      <MovableCard />
-      <MovableCard />
-      <MovableCard />
+      {movableCards.map((card, i) => (
+        <MovableCard key={card.key} ref={refs[i]} />
+      ))}
     </div>
   );
 }
