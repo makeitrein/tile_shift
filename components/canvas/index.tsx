4ee0c2651a8cc58430b1ccb39d160f03d5ff35fe
Jordan Rein <makeitrein@gmail.com>
2020-12-30 21:37:52 -0800

    Imperative yolo

--- components/canvas/index.tsx
+++ components/canvas/index.tsx
@@ -1,26 +1,16 @@
 import * as React from "react";
 import { MovableCard } from "./movable-card";
 
-const movableCards = [{ key: 1 }, { key: 2 }, { key: 3 }];
+const movableCards = [{ id: 1 }, { id: 2 }, { id: 3 }];
 
 export function Canvas() {
   const [target, setTarget] = React.useState();
   const [elementGuidelines, setElementGuidelines] = React.useState([]);
 
-  const refs = React.useMemo(
-    () =>
-      Array.from({ length: movableCards.length }, () =>
-        React.createRef<HTMLDivElement>()
-      ),
-    [movableCards]
-  );
-
-  console.log(refs);
-
   return (
     <div className="container">
       {movableCards.map((card, i) => (
-        <MovableCard key={card.key} ref={refs[i]} />
+        <MovableCard movableCards={movableCards} {...card} key={card.id} />
       ))}
     </div>
   );
--- components/canvas/movable-card.tsx
+++ components/canvas/movable-card.tsx
@@ -3,69 +3,76 @@ import Moveable from "react-moveable";
 
 const borderRadius = "104px 63px 47px 103px / 48.947px 77.053px 121.947px 68px";
 
-export const MovableCard = React.forwardRef(
-  (props, moveRef: React.Ref<HTMLDivElement>) => {
-    const [target, setTarget] = React.useState();
-    const [elementGuidelines, setElementGuidelines] = React.useState([]);
-    const [frame, setFrame] = React.useState({
-      translate: [0, 0],
-    });
+export const MovableCard = ({ id, movableCards }) => {
+  const [target, setTarget] = React.useState<HTMLElement>();
+  const [elementGuidelines, setElementGuidelines] = React.useState([]);
+  const [frame, setFrame] = React.useState({
+    translate: [0, 0],
+  });
 
-    const ref = React.useRef();
+  const cardID = (id: number) => `card-${id}`;
 
-    React.useEffect(() => {
-      setElementGuidelines([
-        // document.querySelector(".nested.rotate"),
-      ]);
-    }, []);
-    return (
-      <div>
-        <div ref={moveRef} style={{ borderRadius }} className="target">
-          Target
-        </div>
-        <Moveable
-          target={moveRef.current}
-          elementGuidelines={elementGuidelines}
-          snappable={true}
-          verticalGuidelines={[0, 200, 400]}
-          horizontalGuidelines={[0, 200, 400]}
-          snapThreshold={5}
-          isDisplaySnapDigit={true}
-          snapGap={true}
-          snapElement={true}
-          snapVertical={true}
-          snapHorizontal={true}
-          snapCenter={false}
-          snapDigit={0}
-          draggable={true}
-          resizable={true}
-          throttleDrag={0}
-          startDragRotate={0}
-          throttleDragRotate={0}
-          zoom={1}
-          origin={true}
-          padding={{ left: 0, top: 0, right: 0, bottom: 0 }}
-          onDragStart={({ set }) => {
-            set(frame.translate);
-          }}
-          onDrag={({ target, beforeTranslate }) => {
-            frame.translate = beforeTranslate;
-            target.style.transform = `translate(${beforeTranslate[0]}px, ${beforeTranslate[1]}px)`;
-          }}
-          onResizeStart={({ setOrigin, dragStart }) => {
-            setOrigin(["%", "%"]);
-            dragStart && dragStart.set(frame.translate);
-          }}
-          onResize={({ target, width, height, drag }) => {
-            const beforeTranslate = drag.beforeTranslate;
-
-            frame.translate = beforeTranslate;
-            target.style.width = `${width}px`;
-            target.style.height = `${height}px`;
-            target.style.transform = `translate(${beforeTranslate[0]}px, ${beforeTranslate[1]}px)`;
-          }}
-        />
-      </div>
+  React.useEffect(() => {
+    setElementGuidelines(
+      movableCards.map((card) => document.getElementById(cardID(card.id)))
     );
-  }
-);
+    setTarget(document.getElementById(cardID(id)));
+  }, []);
+  return (
+    <div>
+      <div
+        contentEditable={true}
+        id={cardID(id)}
+        style={{ borderRadius }}
+        className="target"
+      >
+        Target
+      </div>
+      <Moveable
+        target={target}
+        elementGuidelines={elementGuidelines}
+        snappable={true}
+        verticalGuidelines={[0, 200, 400]}
+        horizontalGuidelines={[0, 200, 400]}
+        snapThreshold={5}
+        isDisplaySnapDigit={true}
+        snapGap={true}
+        snapElement={true}
+        snapVertical={true}
+        snapHorizontal={true}
+        snapCenter={false}
+        snapDigit={0}
+        draggable={true}
+        resizable={true}
+        throttleDrag={0}
+        startDragRotate={0}
+        throttleDragRotate={0}
+        zoom={1}
+        origin={true}
+        padding={{ left: 0, top: 0, right: 0, bottom: 0 }}
+        onDragStart={({ set, inputEvent }) => {
+          if (inputEvent.target.nodeName === "DIV") {
+            stop();
+          }
+          set(frame.translate);
+        }}
+        onDrag={({ target, beforeTranslate }) => {
+          frame.translate = beforeTranslate;
+          target.style.transform = `translate(${beforeTranslate[0]}px, ${beforeTranslate[1]}px)`;
+        }}
+        onResizeStart={({ setOrigin, dragStart }) => {
+          setOrigin(["%", "%"]);
+          dragStart && dragStart.set(frame.translate);
+        }}
+        onResize={({ target, width, height, drag }) => {
+          const beforeTranslate = drag.beforeTranslate;
+
+          frame.translate = beforeTranslate;
+          target.style.width = `${width}px`;
+          target.style.height = `${height}px`;
+          target.style.transform = `translate(${beforeTranslate[0]}px, ${beforeTranslate[1]}px)`;
+        }}
+      />
+    </div>
+  );
+};
